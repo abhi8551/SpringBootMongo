99ed52508d0881317a86ed31cab55461bac806bd
Abhishek Banthia <abhishek20banthia@gmail.com>
2018-12-10 17:12:13 +0530

    To the new repo

--- StoreInformation/src/main/java/com/storeData/dal/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.storeData.dal;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.storeData.modal.Bill;
-
-public interface BillRepository extends MongoRepository<Bill, String>{
-	Bill findByBillId(int BillId);
-}
